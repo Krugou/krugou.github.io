3ea61362e730271fd92dff48a46687bb0855d08f
Krugou <59641229+Krugou@users.noreply.github.com>
2021-04-29 13:01:54 +0300

    12

---
+++ jstraining10/login23/harjoitus1.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "mypassword89",
+    database: "db_henkilöstö"
+})
+con.connect(function(err) {
+    if(err) throw err
+    console.log("Yhdistyi")
+})
+var sql = "delete from projekti where nimi='Teekutsut'"
+con.query(sql, function(err,result,fields){
+    if(err) throw err
+    console.log(result)
+})
+con.end()
---
+++ jstraining10/login23/login.js
@@ -0,0 +1,29 @@
+var mysql = require("mysql")
+var express = require("express")
+var session = require("express-session")
+var path = require("path")
+
+var con = mysql.createConnection({
+    host:       "localhost",
+    user:       "admin",
+    password:   "mypassword89",
+    database:   "kirjautuminen"
+})
+con.connect(function(err){
+    if(err) throw err
+    console.log("Yhdistetty...")
+})
+var app = express()
+
+app.use(session ({
+    secret: "secret",
+    resave: true,
+    saveUninitialized:true
+}))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get("/",function(request,response){
+    response.sendFile(path.join(__dirname + "/login.html"))
+})
