54e2743c296952e4f8e4c02841dbe64d49db5bc4
Krugou <59641229+Krugou@users.noreply.github.com>
2021-08-16 13:45:50 +0300

    sd

--- 2021-08-16/routes/index.js
+++ 2021-08-16/routes/index.js
@@ -1,33 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
-});
-
-const User = sequelize.define('User', {
-  // Model attributes are defined here
-  firstName: { type: DataTypes.STRING, allowNull: false },
-  lastName:  { type: DataTypes.STRING                   }  // allowNull defaults to true
-}, {
-  // Other model options go here
-});
-
-(async () => {
-  await User.sync({ force: true });
-  await User.create({ firstName: 'Christian', lastName: 'Finnberg' });
-})();
-
 
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const users = await User.findAll();
-  const firstuser = users[0].dataValues;
-  console.log(firstuser);
-  res.render('index', { title: 'Express', firstName: firstuser.firstName,lastName: firstuser.lastName });
+ 
+  res.render('index', { title: 'Express', });
 });
 
 module.exports = router;
--- 2021-08-16/routes/users.js
+++ 2021-08-16/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../models')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const users = await User.findAll();
+  const firstuser = users[0].dataValues;
+  console.log(users);
+  res.render('listUsers', { title: 'Express', users1:users });
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
