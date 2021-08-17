e29ccd25b745128eaa008519b5c3f7f2eb09358b
Krugou <59641229+Krugou@users.noreply.github.com>
2021-08-17 12:07:27 +0300

    1

--- testaustoteutus/hellocucumber/features/step_definitions/stepdefs.js
+++ testaustoteutus/hellocucumber/features/step_definitions/stepdefs.js
@@ -1,15 +1,19 @@
 const assert = require('assert');
 const { Given, When,Then} = require('cucumber');
+function isItFriday(today) {
+    if (today === "Friday") {
+        return 'TGIF'
+    } else {
+        return 'Nope'
+    }
+}
 
-Given ('today is Sunday', function() {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-});
+Given ('today is {string}', function(givenDay) {
+    this.today = 'givenDay';
+})
 When('I ask whether it\'s Friday yet', function() {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-});
-Then ('I should be told {string}', function() {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  this.actualAnswer = isItFriday(this.today);
+})
+Then ('I should be told {string}', function(expectedAnswer) {
+   assert.equal(this.actualAnswer,expectedAnswer);
 });
