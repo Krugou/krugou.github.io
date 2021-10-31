d435cd568884a7e95ac48dee65eadfc6778c7f0e
Krugou <59641229+Krugou@users.noreply.github.com>
2021-10-31 20:05:27 +0200

    xcxc

--- blackredcinema3sql/routes/logout-route.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-var router = express.Router();
-/* lopettaa session */
-router.get('/logout', function(req, res) {  
-  req.session.destroy();
-  res.redirect('/login');
-});
-module.exports = router;
