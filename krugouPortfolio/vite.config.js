924778165d3e82addb9f7d1bfd3e9de3d5071e97
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-12 02:25:22 +0300

    Update vite.config.js

--- krugouPortfolio/vite.config.js
+++ krugouPortfolio/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+      format: {
+        comments: true,
+      },
+    },
+  },
+});
