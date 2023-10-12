61b965b0bda9cec4b6dd82d5d67edab384198846
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-12 22:57:48 +0300

    Update MainView.jsx

---
+++ krugouPortfolio/src/components/immigrants/MainView.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainView = () => {
+	return (
+		<div className='main-view'>
+			<h1>Welcome to the Game</h1>
+			<p>
+				This is the main view of the game. Here you can see the game world,
+				interact with characters, and more.
+			</p>
+		</div>
+	);
+};
+
+export default MainView;
