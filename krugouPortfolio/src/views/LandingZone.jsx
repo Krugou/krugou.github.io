56b7771fc094bd5cf2adc7da9d3dc8f3c4a686ba
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-14 14:41:33 +0300

    Update LandingZone.jsx

--- krugouPortfolio/src/views/LandingZone.jsx
+++ krugouPortfolio/src/views/LandingZone.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LandingZone = () => {
 	return (
 		<main>
-			<div className='flex items-center justify-center h-screen bg-gray-200'>
+			<div className='flex items-center justify-center  bg-gray-200'>
 				<h1 className='text-6xl font-bold text-center text-blue-500 animate-flash'>
 					Aleksi Nokelainen
 				</h1>
