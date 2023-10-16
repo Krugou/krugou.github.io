a931dbf1a2b6844fd7dbab4910b3e152d93ddf4c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-16 11:16:55 +0300

    Update Footer.jsx

--- krugouPortfolio/src/views/Footer.jsx
+++ krugouPortfolio/src/views/Footer.jsx
@@ -1,12 +1,11 @@
-
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-    return (
-        <footer>
-            <p>&copy; {currentYear} Krugou Portfolio</p>
-        </footer>
-    );
+	return (
+		<footer className='bg-blue-500 p-4 text-center text-white'>
+			<p className='m-0'>&copy; {currentYear} Krugou Portfolio</p>
+		</footer>
+	);
 };
 
 export default Footer;
