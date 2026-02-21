5cfa93fa3ac976fdccabdd68d141360970f130d8
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2026-02-21 16:19:48 +0200

    Remove monorepo folders in preparation for submodules

--- frontend/tests/simple.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('sanity check', async ({ page }) => {
-  try {
-    await page.goto('/');
-  } catch {}
-  // basic element should exist
-  await expect(page.locator('body')).not.toBeEmpty();
-});
